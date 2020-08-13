b5e03f4c0ad4dbe19b50fc2394adcdc7286205ae
Audrey Dutcher <audrey@rhelmot.io>
2020-08-12 19:54:43 -0700

    rip me and my ability to check my work

--- Randomizer/RandoLogic/StaticRoom.cs
+++ Randomizer/RandoLogic/StaticRoom.cs
@@ -422,7 +422,7 @@ namespace Celeste.Mod.Randomizer {
                         if (entity.Values == null) entity.Values = new Dictionary<string, object>();
                         entity.Values["canChangeMusic"] = false;
                         if (entity.Name == "finalBoss") { // big hack: disable vertical camera locking unless the room is very small horizontally
-                            if (result.Bounds.Width <= 376) {
+                            if (result.Bounds.Width > 376) {
                                 entity.Values["cameraLockY"] = "false";
                             }
                         }
