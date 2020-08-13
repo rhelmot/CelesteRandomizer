cdce0f93761522c42cb7d7a165b62d6d302b497a
Audrey Dutcher <audrey@rhelmot.io>
2020-08-13 11:50:44 -0700

    bundle

--- bundle.sh
+++ bundle.sh
@@ -2,7 +2,7 @@
 
 msbuild
 VERSION=$(cat Randomizer/everest.yaml | grep '^  Version' | cut -d' ' -f 4)
-FILENAME=dist/Randomizer_${VERSION}.zip
+FILENAME=dist/Randomizer_${VERSION}${2}.zip
 rm -f $FILENAME
 cd Randomizer/bin/${1-Debug}
 zip -r ../../../${FILENAME} *
