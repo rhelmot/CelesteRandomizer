556dc7c7a1c5c3aebb73fa3126da0716c78ce9a2
Audrey Dutcher <audrey@rhelmot.io>
2021-01-10 00:05:54 -0700

    Expand map bounds outwards to work around a celeste bug which can come up in custom maps

--- Randomizer/RandoLogic/RandoLogic.cs
+++ Randomizer/RandoLogic/RandoLogic.cs
@@ -339,7 +339,7 @@ namespace Celeste.Mod.Randomizer {
                     num4 = level.Bounds.Bottom;
             }
 
-            map.Bounds = new Rectangle(num1, num2, num3 - num1, num4 - num2);
+            map.Bounds = new Rectangle(num1 - 50, num2 - 50, num3 - num1 + 100, num4 - num2 + 100);
         }
 
         private void SetForeground(MapData map) {
