809323ed1f8946eb41c8c707049ac01227ff68ea
Jeremy Vidaurri <j.x.vidaurri@gmail.com>
2024-08-11 13:00:17 -0500

    fix: update script to add directory if inexistent
    as mentioned in #174

--- bundle.sh
+++ bundle.sh
@@ -4,6 +4,7 @@ dotnet build
 VERSION=$(cat Randomizer/everest.yaml | grep '^  Version' | cut -d' ' -f 4)
 FILENAME=dist/Randomizer_${VERSION}${2}.zip
 rm -f $FILENAME
+mkdir -p dist
 cd Randomizer/bin/${1-Debug}
 zip -r ../../../${FILENAME} *
 echo Finished in ${FILENAME}
