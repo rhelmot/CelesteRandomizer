ab84f62e796fcf854023f32affd2df5774bb19d0
Pandora <131298865+Pandorafoxxx@users.noreply.github.com>
2025-03-16 20:45:25 +0000

    Update RandoConfigFile.cs

--- Randomizer/RandoConfigFile.cs
+++ Randomizer/RandoConfigFile.cs
@@ -326,7 +326,7 @@ namespace Celeste.Mod.Randomizer
         public int? Height { get; set; }
         public float? ScaleX { get; set; }
         public float? ScaleY { get; set; }
-        public color? Color { get; set; } 
+        public Color? Color { get; set; } 
         public List<RandoConfigNode> Nodes { get; set; }
         public Dictionary<string, string> Values { get; set; }
         public string Tile;
