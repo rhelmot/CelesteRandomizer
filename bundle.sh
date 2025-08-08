5ff302543450a4bf0686daeed438559dc93e0ea3
Jeremy Vidaurri <j.x.vidaurri@gmail.com>
2025-08-07 19:03:28 -0500

    Improve build process

--- bundle.sh
+++ bundle.sh
@@ -3,7 +3,7 @@
 dotnet build
 VERSION=$(cat Randomizer/everest.yaml | grep '^  Version' | cut -d' ' -f 4)
 FILENAME=dist/Randomizer_${VERSION}${2}.zip
-rm -f $FILENAME
+rm -f ${FILENAME}
 mkdir -p dist
 cd Randomizer/bin/${1-Debug}
 zip -r ../../../${FILENAME} *
